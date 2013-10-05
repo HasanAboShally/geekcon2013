5797e0bb64c052311329aa53efbd8e57faa8a1d8
Hasan Abo-Shally <HasanAboShally@gmail.com>
2013-10-05 14:55:09 +0300

    working clap

--- nodeLeap/Server.js
+++ nodeLeap/Server.js
@@ -17,30 +17,38 @@ function closeDoor() {
 // ===================================================================================================================== LeapMotion
 
 
-var i = 0;
-var openDoorPattern = ["up", "down", "up"];
-var closeDoorPattern = ["down", "left", "up", "right"];
+//var i = 0;
+//var openDoorPattern = ["up", "down", "up"];
+//var closeDoorPattern = ["down", "left", "up", "right"];
 
-var wsLeapMotion = new WebSocket('ws://localhost:6437')
-    .on('message', function (data, flags) {
-        i++;
+//var wsLeapMotion = new WebSocket('ws://localhost:6437')
+//    .on('message', function (data, flags) {
+//        i++;
 
-        if (i % 3 === 0) {
-            var frame = new LeapFrame(data,
-                                      openDoorPattern, closeDoorPattern,
-                                      openDoor, closeDoor);
-            i = 0;
-        }
-    });
+//        if (i % 3 === 0) {
+//            var frame = new LeapFrame(data,
+//                                      openDoorPattern, closeDoorPattern,
+//                                      openDoor, closeDoor);
+//            i = 0;
+//        }
+//    });
 
 
 // Clap n' Nock
 var COMMANDS = { CLSOE_DOOR: 0, OPEN_DOOR: 1 };
 
-var wsClapping = new WebSocket('ws://192.168.1.206:10000')
+
+//
+
+var stuckIpAdress = "192.168.43.49";
+var wsClapping = new WebSocket('ws://192.168.43.127:10000')
     .on('message', function (command, flags) {
-        switch (command) {
+
+        //console.log(command);
+
+        switch (+command) {
             case COMMANDS.OPEN_DOOR: { openDoor(); } break;
             case COMMANDS.CLSOE_DOOR: { closeDoor(); } break;
         }
+
     });
